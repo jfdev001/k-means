9436ff8be4ef705bde5b7b841a56c02232c1d6ca
Jared Frazier <cscidev001@gmail.com>
2021-11-26 11:17:56 -0600

    init commits with skeleton kmeans

---
+++ kmeans.py
@@ -0,0 +1,98 @@
+"""
+Author: Jared Frazier
+Project: OLA 4
+File: kmeans.py
+Class: CSCI 4350
+Instructor: Dr. Joshua Phillips
+Description: Script for unsupervised clustering of input data using
+K-means algorithm.
+"""
+
+import argparse
+import numpy as np
+
+
+class KMeans:
+    """K-Means Clustering Algorithm."""
+
+    def __init__(self,):
+        """"""
+        pass
+
+    def train(self, num_clusters: int, training_data: np.ndarray) -> None:
+        """"""
+        pass
+
+    def test(self, testing_data: np.ndarray) -> np.ndarray or np.float64:
+        """"""
+        pass
+
+    def __train(self,):
+        """"""
+        pass
+
+    def __test(self,):
+        """"""
+        pass
+
+    def __euclidean_distance(
+            self,
+            arr_1: np.ndarray,
+            arr_2: np.ndarray) -> np.float64:
+        """Returns the 2-norm (euc. distance) of two arrays."""
+
+        return np.linalg.norm(arr_1 - arr_2)
+
+
+def cli(description: str) -> argparse.ArgumentParser:
+    """Command line interface for script."""
+
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument(
+        'random_seed',
+        help='random seed for k-means centroid intialization.',
+        type=int,)
+    parser.add_argument(
+        'num_clusters',
+        help='number of clusters for k-means.',
+        type=int,)
+    parser.add_argument(
+        'train_data_path',
+        help='path to training data for k-means.',
+        type=str,)
+    parser.add_argument(
+        'test_data_path',
+        help='path to testing data for k-means.',
+        type=str,)
+
+    return parser
+
+
+def main():
+
+    # Set up CLI
+    parser = cli('script for train-testing k-means algorithm')
+    args = parser.parse_args()
+
+    # Get data
+    training_data = np.loadtxt(fname=args.train_data_path)
+    testing_data = np.loadtxt(fname=args.test_data_path)
+
+    # Instantiate k-means
+    k_means = KMeans()
+
+    # Train
+    k_means.train(
+        num_clusters=args.num_clusters,
+        training_data=training_data)
+
+    # Test
+    preds = k_means.test(testing_data=testing_data)
+
+    # Print testing outputs
+    pass
+
+
+if __name__ == '__main__':
+    main()
